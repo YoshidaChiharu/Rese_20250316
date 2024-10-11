1adbafb5a0b77fc7e54d3893f0df855df70eff5c
Yoshida Chiharu <gmsp1000.3624@gmail.com>
2024-10-11 17:32:41 +0900

    view,cssの調整（予約完了ページ）

--- resources/views/thanks_reserve.blade.php
+++ resources/views/thanks_reserve.blade.php
@@ -8,6 +8,10 @@
 <div class="thanks-wrapper">
     <div class="thanks-content">
         <p class="thanks-content__text">ご予約ありがとうございます</p>
+        <p class="thanks-content__text">
+            ご登録のメールアドレスへ予約完了メール<br>
+            を送信しましたので、ご確認ください
+        </p>
         <a class="thanks-content__button" href="/">戻る</a>
     </div>
 </div>
